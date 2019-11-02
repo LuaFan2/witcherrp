29480f4952b215b9083066e72d994491492ab340
PatriotRossii <patriotrossii2019@mail.ru>
2019-11-03 00:29:23 +0600

    Minor data save

--- gamemode/plugins/ui/controls/ui_newchar.lua
+++ gamemode/plugins/ui/controls/ui_newchar.lua
@@ -1,80 +1,76 @@
 local PANEL = {}
 
-function PANEL:SetData(name, ftype)
+function PANEL:SetData(name)
 	self.name = name
-	self.type = ftype
 end
 
 function PANEL:GetValue()
-	return self.val
+	return self.entry:GetValue()
 end
 
 function PANEL:SetValue(v)
-	self.val = v
+	self.entry:SetValue(v)
 end
 
 function PANEL:GetData()
-	return self.name, self.type
+	return self.name
 end
 
 function PANEL:Paint(w, h)
 	surface.SetDrawColor( 255, 255, 255, 255 )
 	
-	local n, t = self:GetData()
+	local n = self:GetData()
 	
-	surface.DrawRect(0, 0, w, 40)
-	
+	surface.DrawRect(1, 0, w, 40)
 	draw.SimpleTextOutlined(n, 'ui.witcher', w*0.5, h * 0.3, ui.col.White, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, ui.col.Black)
 end
 
 function PANEL:Init()
-	self:SetSize(449, 60)
-	self.val = ""
+	self:SetSize(349, 60)
 	
-	local entry = ui.Create("DTextEntry", self)
-	entry:SetPos(0,40)
-	entry:SetSize(450,20)
+	self.entry = ui.Create("DTextEntry", self)
+	self.entry:SetPos(1,40)
+	self.entry:SetSize(349,20)
 end
 
 vgui.Register("ui_newchar_field", PANEL, 'Panel')
 
 PANEL = {}
 
-local fields = {{pos = 1, name = "Nick", ftype = "text"}, {pos = 2, name = "Age", ftype = "text"}}
-
 function PANEL:Init()
-	self:SetSize(ScrW(), ScrH())
-	self:MakePopup()
 	
-	table.SortByMember(fields, "pos", true)
-	
-	for k, v in ipairs(fields) do
-		local p = fields[k].pos - 1
-		f = ui.Create("ui_newchar_field", self)
-		f:SetPos(0, p*70)
-		f:SetData(fields[k].name, fields[k].ftype)
-	end
 end
 
 function PANEL:Paint(w, h)
-	surface.SetMaterial(Material( "black.jpg" ))
-	surface.SetDrawColor( 255, 255, 255, 255 )
-	surface.DrawTexturedRect(0, 0, w, h)
 	
-	surface.DrawLine(450, 0, 450, h)
-	surface.DrawLine(800, 0, 800, h)
 end
 
 vgui.Register("ui_newchar_blank", PANEL, 'Panel')
 
 PANEL = {}
 
-function PANEL:Init()
-	local blank = ui.Create("ui_newchar_blank", self)
+function PANEL:Init()	
+	self:SetSize(ScrW(), ScrH())
+	self:MakePopup()
+	
+	f = ui.Create("ui_newchar_field", self)
+	f:Center()
+	local x, y = f:GetPos()
+	f:SetPos(x, 0)
+	f:SetData("Name")
+	
+	model = ui.Create("DModelPanel", self)
+	model:SetSize(400, 800)
+	model:Center()
+	model:SetDirectionalLight( BOX_TOP, Color( 255, 255, 255 ) )
+	model:SetDirectionalLight( BOX_FRONT, Color( 255, 255, 255 ) )
+	model:SetModel(LocalPlayer():GetModel())
 end
 
 function PANEL:Paint(w, h)
-
+	surface.SetMaterial(Material( "black.jpg" ))
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	surface.DrawTexturedRect(0, 0, w, h)
 end
 
-vgui.Register("ui_newchar", PANEL, 'Panel')
+vgui.Register("ui_newchar", PANEL, 'EditablePanel')
