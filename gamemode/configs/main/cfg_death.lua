86753014e9caea212f293cce5a5210218190ad4d
LuaFan2 <kremlebotone@gmail.com>
2019-11-11 16:06:25 +0500

    Trash cleanup #2

--- gamemode/configs/main/cfg_death.lua
+++
@@ -1,6 +0,0 @@
-rp.Config = rp.Config or {}
-
-rp.Config.Death = rp.Config.Death or {}
-
--- revive time. 1 for moment revive(0 disables rebirth). You can disable this in hud settings
-rp.Config.Death.TimeRevive = 5
